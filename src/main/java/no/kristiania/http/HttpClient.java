8b7b1c6599ed903dbd924fd936999d06d00e7205
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-06 16:35:50 +0200

    test shouldReturn404StatusCode (green)

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -6,12 +6,30 @@ import java.net.Socket;
 
 public class HttpClient {
 
-    public HttpClient(String host, int port, String requestTarget) {
-        
+    private final int statusCode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+
+        socket.getOutputStream().write(
+                ("GET " + requestTarget + " HTTP/1.1\r\n" +
+                        "Connection: close\r\n" +
+                        "Host: " + host + "\r\n" +
+                        "\r\n").getBytes()
+        );
+
+        StringBuilder result = new StringBuilder();
+        InputStream in = socket.getInputStream();
+        int c;
+        while ((c = in.read()) != -1) {
+            result.append((char)c);
+        }
+        String responseMessage = result.toString();
+        this.statusCode = Integer.parseInt(responseMessage.split(" ")[1]);
     }
     
     public int getStatusCode() {
-        return 200;
+        return statusCode;
     }
 
     public static void main(String[] args) throws IOException {
@@ -19,6 +37,7 @@ public class HttpClient {
         
         socket.getOutputStream().write(
                 ("GET /html HTTP/1.1\r\n" +
+                        "Connection: close\r\n" +
                         "Host: httpbin.org\r\n" +
                         "\r\n").getBytes()
                 
